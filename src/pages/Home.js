a9a2c6dc367c8e6ee409801bfce4ec20fe39bf10
ferchuqui <fernando.farji@gmail.com>
2020-12-19 03:33:54 -0300

    Implementar y probar las conexiones

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
   const view = `
   <div class="Characters">
-    <article class="Character-item>
+    <article class="Character-item">
       <a href="#/1/">
       <img src= "image" alt="name">
       <h2>Name</h2>
